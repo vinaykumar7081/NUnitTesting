ce6bc577cfb6b16f031ba692bd7d7bd75f90a7bd
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-16 12:29:32 +0530

    [VINAY] Add: Displaying Reverse Number

--- NUnitTestingProject/Program.cs
+++ NUnitTestingProject/Program.cs
@@ -8,7 +8,7 @@ public class Program
         bool check = true;
         while (check)
         {
-            Console.WriteLine(" Enter Zero To Stop The Excution 0\n Dispaly FibbonacciSeries 1\n Display Perfect Number 2\n Checking Prime or Not 3\n");
+            Console.WriteLine(" Enter Zero To Stop The Excution 0\n Dispaly FibbonacciSeries 1\n Display Perfect Number 2\n Checking Prime or Not 3\n Reversig Number 4\n");
             Console.WriteLine("Enter the Above Option");
             int option=Convert.ToInt32(Console.ReadLine());
             switch (option)
@@ -24,6 +24,10 @@ public class Program
                 case 3:
                     PrimeNumber prime = new PrimeNumber();
                     prime.Prime();
+                    break;
+                case 4:
+                    ReverseNumber  reverse = new ReverseNumber();
+                   reverse .NumberReversing ();
                     break;
                 case 0:
                    check = false;
---
+++ NUnitTestingProject/ReverseNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTestingProject
+{
+    public  class ReverseNumber
+    {
+        int rem = 0 ,sum=0;
+        public void NumberReversing()
+        {
+            Console.WriteLine("Enter a Number to reversing");
+            int number=Convert.ToInt32(Console.ReadLine());
+            while (number > 0)
+            {
+                rem = number % 10;
+                sum = sum * 10 + rem;
+                number = number / 10;
+            }
+            Console.WriteLine("Reverse numbere is:--->"+sum);
+        }
+    }
+}
