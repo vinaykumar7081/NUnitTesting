2d5d66bc12c36abd6dd45f76494fe07d9ef54954
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-16 13:35:54 +0530

    [VINAY] Add: Converting The Temparature

--- NUnitTestingProject/Program.cs
+++ NUnitTestingProject/Program.cs
@@ -8,7 +8,7 @@ public class Program
         bool check = true;
         while (check)
         {
-            Console.WriteLine(" Enter Zero To Stop The Excution 0\n Dispaly FibbonacciSeries 1\n Display Perfect Number 2\n Checking Prime or Not 3\n Reversig Number 4\n Day Of Week 5\n");
+            Console.WriteLine(" Enter Zero To Stop The Excution 0\n Dispaly FibbonacciSeries 1\n Display Perfect Number 2\n Checking Prime or Not 3\n Reversig Number 4\n Day Of Week 5\n Temparature Conversion 6\n");
             Console.WriteLine("Enter the Above Option");
             int option=Convert.ToInt32(Console.ReadLine());
             switch (option)
@@ -32,6 +32,11 @@ public class Program
                 case 5:
                     DisplayDayOfWeek  day = new DisplayDayOfWeek();
                     day.DayOfWeek();
+                    break;
+                case 6:
+                    TemparatureConversion  temparature = new TemparatureConversion();
+                    temparature.CelsiusToFahrenhiet ();
+                    temparature.FahrenhietToCelsius ();
                     break;
                 case 0:
                    check = false;
---
+++ NUnitTestingProject/TemparatureConversion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTestingProject
+{
+    public class TemparatureConversion
+    {
+        public void CelsiusToFahrenhiet()
+        {
+            Console.WriteLine("Enetr the Temparature in Celsius to Convert: ");
+            int celsius = Convert.ToInt32(Console.Read());
+            float fahrenhiet = ((float)celsius * 9 / 5) + 32;
+            Console.WriteLine("Converted Temparature is:->" + fahrenhiet + "F");
+        }
+        public void FahrenhietToCelsius()
+        {
+            Console.WriteLine("Enetr the Temparature in Fahrenheit to Convert: ");
+            int fahrenhiet = Convert.ToInt32(Console.Read());
+            float celsius = (float)((fahrenhiet - 32) * 5 / 9);
+            Console.WriteLine("Converted Temparature is:->" + celsius + "C");
+        }
+    }
+
+}
+
