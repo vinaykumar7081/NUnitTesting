44840e491a70634737aaae5f3db1bf91c514d942
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-16 12:01:47 +0530

    [VINAY] Add: Displaying Fibbonacci Series

---
+++ NUnitTestingProject/FibbonacciSeries.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTestingProject
+{
+    public class FibbonacciSeries
+    {
+        int first_Num = 0, second_Num = 1, third_Num;
+        public void Fibbonacci()
+        {
+
+            Console.WriteLine("Enter the length of the Series");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Fibbonacci Siries is:->");
+            Console.Write(" " + first_Num);
+            Console.Write(" " + second_Num);
+            for (int i = 1; i <= num; i++)
+            {
+                third_Num = first_Num + second_Num;
+                Console.Write(" " + third_Num);
+                first_Num = second_Num;
+                second_Num = third_Num;
+            }
+            Console.WriteLine(" ");
+        }
+    }
+}
---
+++ NUnitTestingProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnitTestingProject;
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine(" ********* Welcome in The Basic Logical Programming World ********");
+        bool check = true;
+        while (check)
+        {
+            Console.WriteLine(" Enter Zero To Stop The Excution 0\n Dispaly FibbonacciSeries 1\n");
+            Console.WriteLine("Enter the Above Option");
+            int option=Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FibbonacciSeries  series = new FibbonacciSeries();
+                    series.Fibbonacci();
+                    break;
+                case 0:
+                   check = false;
+                    break;
+            }
+        }
+    }
+}
